c86bf839f69d24f0b5480feb298399d55c4d0693
marcosrafaellsousa <marcosrafaellsousa@gmail.com>
2018-09-10 20:44:55 -0300

    Aplicacao.java adicionado na aula-3/semana

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,9 @@
+package br.ufg.inf.mf.pratica;
+
+import java.time.LocalDate;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		System.out.println(LocalDate.now().getDayOfWeek());
+	}
+}
